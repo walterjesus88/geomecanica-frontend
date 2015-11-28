2b9044cdd38356fa36658a864613ab9ad130476e
wjesus88 <wjesus88@gmail.com>
2015-11-28 16:51:47 -0500

    agregando

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -87,52 +87,13 @@ function(Labor, $scope, $state) {
     enableFriends: true
   };
 
-  riesgo = this;
+  createRiesgo = this;
   $scope.$parent.index.isAuth = true;
 
-  riesgo.preguntasList = Pregunta.query();
-  console.log(riesgo.preguntasList);
 
-  // home.questionsList = Question.query();
-  // console.log(home.questionsList);
-  riesgo.guardarinspeccion = function() {
-
-    var inspeccion = new Inspeccion();
-    inspeccion.periodo = '5';
-    inspeccion.created = 2015-05-05;
-    inspeccion.updated = 2015-05-05;    
-    inspeccion.fecha = 2015-05-05;    
-    inspeccion.$save(function() {
-      //$state.go('tabsUsers.users');
-      console.log('hoolla');
-    });
-    //riesgo.inps_id=7;
-    // riesgo.periodo='7';
-    // riesgo.created='2015-05-05';
-    // riesgo.updated='2015-08-08';
-
-    // Inspeccion.guardarDatos(riesgo.periodo,riesgo.created,riesgo.updated)
-    // .then(function(data) {  
-    //   console.log('si'); 
-    // })
-    // .catch(function(err) {
-    //   console.log('no');   
-    // });
-  }
-
-  // $scope.preguntasList = [
-  //   { text: "Wireless", checked: true },
-  //   { text: "GPS", checked: false },
-  //   { text: "GPS1", checked: false },
-  //   { text: "GPS2", checked: false },
-  //   { text: "GPS3", checked: false },
-  //   { text: "Bluetooth", checked: false }
-  // ];
-
-  //Para el Datepicker//
-  $scope.datepickerObject = {};
-  $scope.datepickerObject.inputDate = new Date(2015, 6, 30);
-
+    //Para el Datepicker//
+  createRiesgo.datepickerObject = {};
+  createRiesgo.datepickerObject.inputDate = new Date;
   $scope.datepickerObject = {
       titleLabel: 'Title',  //Optional
       todayLabel: 'Today',  //Optional
@@ -141,7 +102,7 @@ function(Labor, $scope, $state) {
       setButtonType : 'button-assertive',  //Optional
       todayButtonType : 'button-assertive',  //Optional
       closeButtonType : 'button-assertive',  //Optional
-      inputDate: $scope.datepickerObject.inputDate,   //Optional
+      inputDate: createRiesgo.datepickerObject.inputDate,   //Optional
       mondayFirst: true,  //Optional
       disabledDates: disabledDates, //Optional
       weekDaysList: weekDaysList, //Optional
@@ -155,7 +116,7 @@ function(Labor, $scope, $state) {
       callback: function (val) {  //Mandatory
         datePickerCallback(val);
       },
-      dateFormat: 'dd-MM-yyyy', //Optional
+      dateFormat: 'yyyy-MM-d', //Optional
       closeOnSelect: false, //Optional
   };
 
@@ -164,7 +125,7 @@ function(Labor, $scope, $state) {
       if (typeof(val) === 'undefined') {
         console.log('No date selected');
       } else {
-        $scope.datepickerObject.inputDate = val;
+        createRiesgo.datepickerObject.inputDate = val;
         console.log('Selected date is : ', val)
       }
   };
@@ -180,6 +141,64 @@ function(Labor, $scope, $state) {
 
   var weekDaysList = ["Sun", "Mon", "Tue", "Wed", "thu", "Fri", "Sat"];
   var monthList = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+
+  createRiesgo.tipolabor = [{ text: "Avance", value:'AVANCE', },{ text: "Explotacion", value:'EXPLOTACION', }, ];
+  createRiesgo.insp_tlabor= {tipolabor: 'AVANCE' };
+
+  createRiesgo.preguntasList = Pregunta.query();
+
+  createRiesgo.guardia = [{ text: "Dia", value:'DIA', },{ text: "Noche", value:'NOCHE', }, ];
+  createRiesgo.insp_guard = {guardia: 'DIA' };
+
+
+  createRiesgo.empresa = [{ text: "MILPO", value:'MILPO'},{text: "E1", value:'E1'},{text: "E2", value:'E2'}];
+  createRiesgo.insp_empresa = {empresa: 'MILPO' };
+
+  createRiesgo.opcionlabor = [{ text: "Labor con sostenimiento", value:'LCS'},{text: "Labor con sostenimiento", value:'LSS'}];
+  createRiesgo.inps_OL = {olabor: 'LCS' };
+
+  createRiesgo.geomecanica = [{ text: "Cumplio", value:'TRUE'},{text: "No Cumplio", value:'FALSE'}];
+  createRiesgo.insp_recomendacion = {rgeo: 'TRUE' };
+
+  createRiesgo.install = [{ text: "Correcta", value:'TRUE'},{text: "Incorrecta", value:'FALSE'}];
+  createRiesgo.insp_install = {resp: 'TRUE' };
+
+  createRiesgo.guardarinspeccion = function() {    
+    
+    console.log(createRiesgo.datepickerObject.inputDate);      
+    fecha = new Date(createRiesgo.datepickerObject.inputDate);
+    day=fecha.getDate();
+    month=fecha.getMonth()+1;
+    year=fecha.getFullYear();    
+    if (month.toString().length < 2) 
+    {
+      month = '0' + month;
+    }
+    if (day.toString().length < 2) 
+    {
+      day = '0' + day;
+    }                           
+    fecha=year+"-"+month+"-"+day;
+
+    console.log(fecha);     
+
+    var inspeccion = new Inspeccion();
+    inspeccion.periodo = createRiesgo.insp_guard.guardia;  
+    inspeccion.recomendacion = createRiesgo.insp_recomendacion.rgeo;  
+    inspeccion.instalacion = createRiesgo.insp_install.resp;  
+    inspeccion.tipo = createRiesgo.insp_tlabor.tipolabor;  
+    inspeccion.comentario = createRiesgo.comment;
+    inspeccion.ancho_real = createRiesgo.ancho_real;  
+    inspeccion.alto_real= createRiesgo.alto_real;
+    inspeccion.fecha= fecha;
+
+    inspeccion.$save(function() {
+      //$state.go('tabsUsers.users');
+      console.log('hoolla');
+    });
+  }
+
+
 }])
 
 
