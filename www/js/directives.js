e28eda805bc6b010f27aa7edaba572355a25e701
wjesus <wjesus88@gmail.com>
2015-12-01 12:58:26 -0500

    *

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,8 +1,58 @@
 angular.module('app.directives', [])
 
-// .directive('blankDirective', [function(){
+.directive('blankDirective', [function(){
 
-// }])
+}])
+
+
+.directive('multiSelectDate', [function($filter) {
+	return {
+		restrict:'E',
+		require:'?ngModel',
+		templateUrl: 'multiSelectDate',
+		link: function(scope, element, attrs, ngModel) { 
+			if (!ngModel) return;
+
+			ngModel.$render = function() {
+		        scope.date = {
+		            day: $filter('date')(ngModel.$viewValue, 'dd'),
+          			month: $filter('date')(ngModel.$viewValue, 'MM'),
+          			year: $filter('date')(ngModel.$viewValue, 'yyyy')
+		        };
+      		};
+
+      		scope.selects = {
+	       		days: function() {
+		          // Get number of days based on month + year 
+		          // (January = 31, February = 28, April = 30, February 2000 = 29) or 31 if no month selected yet
+		          var nbDays = new Date(scope.date.year, scope.date.month, 0).getDate() || 31;
+
+		          var daysList = [];
+		          for (var i = 1; i <= nbDays; i++) {
+		            var iS = i.toString();
+		            daysList.push((iS.length < 2) ? '0' + iS : iS); // Adds a leading 0 if single digit
+		          }
+		          return daysList;
+		        },
+		        months: function() {
+		          var monthList = [];
+		          for (var i = 1; i <= 12; i++) {
+		            var iS = i.toString();
+		            monthList.push((iS.length < 2) ? '0' + iS : iS); // Adds a leading 0 if single digit
+		          }
+		          return monthList;
+		        },
+		        years: function() {
+		          var yearsList = [];
+		          for (var i = endYear; i >= startYear; i--) {
+		            yearsList.push(i.toString());
+		          }
+		          return yearsList;
+		        }
+	      };
+		}
+	}
+}])
 
 .directive('ionSelect',[function(){
 	'use strict';
